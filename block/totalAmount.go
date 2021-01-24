a1e2cf34620ef200cd7772f05b8e7f0c4cd16215
EugeniaKol <jennichka10155@gmail.com>
2021-01-24 17:53:30 +0200

    introducing block calculation methods

---
+++ block/totalAmount.go
@@ -0,0 +1,29 @@
+package block
+
+import (
+	"errors"
+	"math"
+	"math/big"
+)
+
+func (block Block) CalculateTotal() (Stats, error) {
+	var res Stats
+	for _, item := range block.Result.Transactions {
+		res.Transactions++
+		val := item.Value[2:]
+
+		num, _ := new(big.Int).SetString(val, 16)
+		f := new(big.Float).SetInt(num)
+		wei, _ := f.Float64()
+		//fmt.Println(acc)
+
+		//eth, err := strconv.ParseUint(val, 16, 64)
+
+		if num == nil {
+			return Stats{}, errors.New("cant parse value")
+		}
+		res.Amount += wei / math.Pow(10, 18)
+	}
+
+	return res, nil
+}
