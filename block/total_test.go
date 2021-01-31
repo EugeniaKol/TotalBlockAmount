fa4d6ff781cf985a732778de57987be5075f6253
EugeniaKol <jennichka10155@gmail.com>
2021-02-01 01:49:02 +0200

    fixing nested tests

--- block/total_test.go
+++ block/total_test.go
@@ -5,21 +5,13 @@ import "testing"
 func TestBlock_CalculateTotal(t *testing.T) {
 	for _, testCase := range []struct {
 		name     string
-		args     Block
+		args     []string
 		expected Stats
 		exError  error
 	}{
 		{
 			name: "Basic",
-			args: Block{
-				Result: Result{
-					Transactions: []Transaction{{
-						Value: "0x13",
-					}, {
-						Value: "0x54",
-					}},
-				},
-			},
+			args: []string{"0x13", "0x54"},
 			expected: Stats{
 				Transactions: 2,
 				Amount:       1.03e-16,
@@ -29,17 +21,7 @@ func TestBlock_CalculateTotal(t *testing.T) {
 
 		{
 			name: "Basic#2",
-			args: Block{
-				Result: Result{
-					Transactions: []Transaction{{
-						Value: "0x00",
-					}, {
-						Value: "0x57cf",
-					}, {
-						Value: "0x847",
-					}},
-				},
-			},
+			args: []string{"0x00", "0x57cf", "0x847"},
 			expected: Stats{
 				Transactions: 3,
 				Amount:       2.4597999999999998e-14,
@@ -48,7 +30,14 @@ func TestBlock_CalculateTotal(t *testing.T) {
 		},
 	} {
 		t.Run(testCase.name, func(t *testing.T) {
-			res, err := testCase.args.CalculateTotal()
+			var block Block
+
+			for _, value := range testCase.args {
+				block.Result.Transactions = append(block.Result.Transactions, Transaction{Value: value})
+			}
+
+			res, err := block.CalculateTotal()
+
 			if res != testCase.expected {
 				t.Error("expected ", testCase.expected, " got ", res)
 			} else if testCase.exError != err {
