ad3bf67f437f38b9e1467f46bd629f237d35999c
EugeniaKol <jennichka10155@gmail.com>
2021-02-01 02:21:26 +0200

    fixing incorrect order of variable checking and usage

--- block/total.go
+++ block/total.go
@@ -13,12 +13,12 @@ func (block Block) CalculateTotal() (Stats, error) {
 		val := item.Value[2:]
 
 		num, _ := new(big.Int).SetString(val, 16)
-		f := new(big.Float).SetInt(num)
-		wei, _ := f.Float64()
-
 		if num == nil {
 			return Stats{}, errors.New("cant parse value")
 		}
+
+		f := new(big.Float).SetInt(num)
+		wei, _ := f.Float64()
 		res.Amount += wei / math.Pow(10, 18)
 	}
 
